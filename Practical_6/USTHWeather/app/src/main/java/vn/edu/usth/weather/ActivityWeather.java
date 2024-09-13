6f5a5ff8e4b789847d25fdffc2e1c845ea3775f6
NguyenTriAnhVu <nguyenvutri2206@gmail.com>
2024-09-13 22:47:31 +0700

    Practical 6

---
+++ Practical_6/USTHWeather/app/src/main/java/vn/edu/usth/weather/ActivityWeather.java
@@ -0,0 +1,24 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityWeather extends AppCompatActivity {
+
+    private static final String TAG = "WeatherActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(TAG, "onCreate called");
+
+        ForecastFragment forecastFragment = new ForecastFragment();
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.weather_condition, forecastFragment)
+                .commit();
+    }
+
+}
---
+++ Practical_6/USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherFragment.java
@@ -0,0 +1,23 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WeatherFragment extends Fragment {
+
+    public WeatherFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
+}
